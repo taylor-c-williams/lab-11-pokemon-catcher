75531bb124ea8f2f1c9ac724ae1eb824421497ae
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2021-08-31 22:21:09 -0700

    appjs wireframe

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
-// import functions and grab DOM elements
+letPokeCaught = 0
+pokeCaught ++ <= on click/catch
+if pokeCaught > 10, redirect to results
 
-// initialize global state
+renderNewPokemon()
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+getRandomPokemon(){
+  random math function
+  return array/random pokemon
+}
+
+encounterPokemon(id){
+  getPokedex()
+  ++ times encountered
+  if !encountered, newObject(id: 5, encountered:1 caught:0)
+  setPokedex()
+}
+
+setPokedex(pokedex){
+  make stringy, store in local
+}
+
+getPokedex(){
+  de-string, get from local
+}
+
+catchPokemon(id){
+  getPokedex()
+  caught ++ (in local)
+  setPokedex()
+}
+
+howManyCaughtSoFar()
