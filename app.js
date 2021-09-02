c413b8240807c754b89d376abc0bfe1077e6d7d6
taylor-c-williams <taylor.callan.williams@gmail.com>
2021-09-02 08:54:44 -0700

    app js framework

---
+++ app.js
@@ -0,0 +1,36 @@
+import { encounterPokemon } from "./local-storage-utils"
+
+// import functions
+catch, encounterPokemon, get random
+//initalize state
+let caught = 0;
+
+//get 3 pkmn in array
+//define render function:
+function renderNewPokemon(){
+
+}
+//for each 3 labels
+
+//label's index to grab pokemon item
+
+//first child is radio button
+
+// second child is image tag 
+//change it's src
+
+renderNewPokemon()
+
+//set event listeners on form
+//inside event listener:
+
+//disable default
+//increment caught value ++
+//set formdata const
+//set const for pokemon ID data
+//data.get
+
+// call catch pokemon w ID
+//if caught > 10, redirect
+//else renderNewPokemon
+
