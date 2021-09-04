6ecb4fa8c93fcc1cb207c528c62ae6391693cbd2
taylor-c-williams <taylor.callan.williams@gmail.com>
2021-09-04 16:31:24 -0700

    encounterPokemon passing

--- test/example.test.js
+++ test/example.test.js
@@ -1,10 +1,12 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 
-import { setPokedex, getPokedex } from '../utils.js';
+import { setPokedex, getPokedex, encounterPokemon } from '../utils.js';
 
 const test = QUnit.test;
 
+
+//Set
 test('setPokemon should store JSON data accurately', (expect) => {
 
     const pokemon = [
@@ -14,28 +16,42 @@ test('setPokemon should store JSON data accurately', (expect) => {
    
     const stringyPokemon = localStorage.getItem('POKEMON');
     const unstringyPokemon = JSON.parse(stringyPokemon);
+
     const expected = unstringyPokemon;
-        //Act 
-    // Call the function you're testing and set the result to a const
     const actual = setPokedex(pokemon);
-    //Expect
-    // Make assertions about what is expected versus the actual result
+
     expect.equal(actual, expected);
 });
 
+//Get
 test('Get should retrieve JSON data accurately', (expect) => {
     const pokemonArray = [
         { id:5,
             quantity:25 }
     ];
+
     const lemon = JSON.stringify(pokemonArray);  
     localStorage.setItem('POKEDEX', lemon);
-
     localStorage.getItem('POKEDEX');
  
-
     const expected = pokemonArray; 	
     const actual = getPokedex();
 
     expect.deepEqual(actual, expected);
 });
+
+//Encounter
+test('encounterPokemon should increase encountered property', (expect) => {
+    const pokemonArray = [
+        { id:5,
+            quantity:25, 
+            encountered:2
+        }
+    ];
+
+    const expected = pokemonArray.id; 
+    const actual = encounterPokemon(Number(5));
+
+    expect.equal(actual, expected);
+});
+
