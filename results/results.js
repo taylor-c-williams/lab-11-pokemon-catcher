d33550c4cf4e6d1e314564835b45e0e644679af9
taylor-c-williams <taylor.callan.williams@gmail.com>
2021-09-06 13:35:47 -0700

    working chart

--- results/results.js
+++ results/results.js
@@ -4,42 +4,48 @@ import { getPokedex } from '../utils.js';
 const ctx = document.getElementById('myChart');
 const pokedex = getPokedex();
 
-const arrayOfNames = pokedex.filter(pokemon => pokemon.captured > 0)
-    .map (pokemon => pokemon.name);
-console.log (arrayOfNames);
+const arrayOfNames = pokedex
+    .filter(pokemon => pokemon.captured > 0)
+    .map(pokemon => pokemon.name);
 
-const arrayOfCaptures = pokedex.filter(pokemon => pokemon.captured > 0)
-    .map (pokemon => pokemon.captured);
-console.log (arrayOfCaptures);
+const arrayOfCaptures = pokedex
+    .filter(pokemon => pokemon.captured > 0)
+    .map(({ captured }) => captured);
 
-const arrayOfEncounters = pokedex.map (encountered => encountered);
-console.log (arrayOfEncounters);
+const arrayOfEncounters = pokedex
+    .filter(pokemon => pokemon.encountered > 0)
+    .map(({ encountered }) => encountered);
 
-new Chart(ctx, { 
-    type: 'radar',
+new Chart(ctx, {
+    type: 'bar',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [121, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
+        labels: arrayOfNames,
+        datasets: [
+            {
+                label: 'captured',
+                data: arrayOfCaptures,
+                backgroundColor: [
+                    'orange',
+
+                ],
+    
+                borderWidth: 5
+            },
+            {
+                label: 'encountered',
+                data: arrayOfEncounters,
+                backgroundColor: [
+                    'red',
+                    'pink',
+                    'yellow',
+                    'green',
+                    'blue',
+                    'purple'
+                ],
+
+                borderWidth: 1
+            }
+        ]
     },
     options: {
         scales: {
